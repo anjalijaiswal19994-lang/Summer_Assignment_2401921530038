45ff6fe68ca60da8f5a64da81619b48a2f8e0bd3
Anjali Jaiswal <anjalijaiswal19994@gmail.com>
2026-06-01 22:24:49 +0530

    Fixed mistakes in code

--- Week_1/Day_1/Twosum.cpp
+++ Week_1/Day_1/Twosum.cpp
@@ -4,17 +4,17 @@ public:
     vector<int> twoSum(vector<int> &nums, int target)
     {
         map<int, int> mpp;
-        int x = nums.size();
+        int n = nums.size();
         for (int i = 0; i < n; i++)
         {
-            int nums = num[i];
+            int num = nums[i];
             int mppneeded = target - num;
             if (mpp.find(mppneeded) != mpp.end())
             {
-                return (mpp[mppneeded], i);
+                return {mpp[mppneeded], i};
             }
             mpp[num] = i;
         }
-        return (-1, -1);
+        return {-1, -1};
     }
 };
