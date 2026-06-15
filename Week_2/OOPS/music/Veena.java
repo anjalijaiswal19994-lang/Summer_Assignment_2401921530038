19ee5ef5736ee6d73b9677317ead59511cf0ce3a
Anjali Jaiswal <anjalijaiswal19994@gmail.com>
2026-06-15 20:25:41 +0530

    Fix Week_1 folder structure

--- Week_2/OOPS/music/Veena.java
+++ Week_2/OOPS/music/Veena.java
@@ -3,8 +3,9 @@ package music.string;
 import music.Playable;
 
 public class Veena implements Playable {
+
     @Override
     public void play() {
-        System.out.println("Veena is playing");
+        System.out.println("Veena is playing melodiously");
     }
 }
