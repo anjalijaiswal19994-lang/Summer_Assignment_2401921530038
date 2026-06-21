66e0990ea217a4564be332e34602992690430cf3
Anjali Jaiswal <anjalijaiswal19994@gmail.com>
2026-06-21 14:55:01 +0530

    Add compartment assignment

---
+++ Week_3/OOPS/TestCompartment.java
@@ -0,0 +1,72 @@
+import java.util.Random;
+
+// Abstract class
+abstract class Compartment {
+    public abstract String notice();
+}
+
+// FirstClass class
+class FirstClass extends Compartment {
+    @Override
+    public String notice() {
+        return "First Class Compartment: Reserved for premium passengers.";
+    }
+}
+
+// Ladies class
+class Ladies extends Compartment {
+    @Override
+    public String notice() {
+        return "Ladies Compartment: Reserved for women passengers only.";
+    }
+}
+
+// General class
+class General extends Compartment {
+    @Override
+    public String notice() {
+        return "General Compartment: Open for all passengers.";
+    }
+}
+
+// Luggage class
+class Luggage extends Compartment {
+    @Override
+    public String notice() {
+        return "Luggage Compartment: For carrying luggage and parcels.";
+    }
+}
+
+// Test class
+public class TestCompartment {
+    public static void main(String[] args) {
+        Compartment[] compartments = new Compartment[10];
+        Random random = new Random();
+
+        for (int i = 0; i < compartments.length; i++) {
+            int choice = random.nextInt(4) + 1;
+
+            switch (choice) {
+                case 1:
+                    compartments[i] = new FirstClass();
+                    break;
+                case 2:
+                    compartments[i] = new Ladies();
+                    break;
+                case 3:
+                    compartments[i] = new General();
+                    break;
+                case 4:
+                    compartments[i] = new Luggage();
+                    break;
+            }
+        }
+
+        System.out.println("Compartment Notices:\n");
+
+        for (int i = 0; i < compartments.length; i++) {
+            System.out.println("Compartment " + (i + 1) + ": "
+                    + compartments[i].notice());
+        }
+    }
+}
